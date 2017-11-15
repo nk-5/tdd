4eff04b30acc068b267640d70810317edb01af98
nkws <keigo_nakagawa@voyagegroup.com>
2017-11-15 12:31:05 +0900

    setup tdd enviroment

---
+++ tdd/src/main/money/Money.java
@@ -0,0 +1,7 @@
+package money;
+
+/**
+ * Created by ke-nakagawa on 2017/11/08.
+ */
+public class Money {
+}
---
+++ tdd/src/tests/money/DollarTest.java
@@ -0,0 +1,10 @@
+package money;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by ke-nakagawa on 2017/11/08.
+ */
+class DollarTest {
+
+}
